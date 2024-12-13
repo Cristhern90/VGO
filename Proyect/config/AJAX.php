78afde2e09372291c50b1fea79ce5e823f980d27
Cristian Hernan de la Rosa <cristhern20@gmail.com>
2024-12-13 10:42:09 +0100

    Create Config Files

---
+++ Proyect/config/AJAX.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+/**
+ * Description of AJAX
+ *
+ * @author Cristian
+ */
+
+include './VGO.php';
+
+class AJAX extends VGO {
+    //put your code here
+}
---
+++ Proyect/config/API.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+/**
+ * Description of API
+ *
+ * @author Cristian
+ */
+
+include './VGO.php';
+
+class API extends VGO {
+    //put your code here
+}
