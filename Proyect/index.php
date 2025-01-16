e564c0d917fcddbf62fd9bd4370524d2a2da799e
Cristian Hernan de la Rosa <cristhern20@gmail.com>
2025-01-16 22:06:26 +0100

    Page include

--- Proyect/index.php
+++ Proyect/index.php
@@ -1,8 +1,13 @@
 <?php
 session_start();
 $user_id = 0;
+$page = "login";
 if (isset($_COOKIE["user_id"])) {
     $user_id = $_COOKIE["user_id"]; // get user id
+    $page = "home";
+    if (iseet($_GET["page"])) {
+        $page = $_GET["page"]; // get page
+    }
 }
 ?>
 <html>
@@ -12,6 +17,7 @@ if (isset($_COOKIE["user_id"])) {
     <body>
         <?php
         include './includes/header.php';
+        include './front/' . $page . '/index.php';
         include './includes/footer.php';
         ?>
     </body>
