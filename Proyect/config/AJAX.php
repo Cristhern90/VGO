4674d89eb72ce2a59e6d2784706a947136a16bc6
Cristian Hernan de la Rosa <cristhern20@gmail.com>
2025-02-13 12:02:49 +0100

    AJAX insert single row function

--- Proyect/config/AJAX.php
+++ Proyect/config/AJAX.php
@@ -10,11 +10,42 @@
  *
  * @author Cristian
  */
-
 include './VGO.php';
 
 class AJAX extends VGO {
-    
-    public $result = array("responce"=>"","errorCode"=>0,"errorMessage"=>"","alert"=>0,"reload"=>0,"newLocation"=>"");//array to response ajax conection
-    
+
+    public $result = array("responce" => "", "errorCode" => 0, "errorMessage" => "", "alert" => 0, "reload" => 0, "newLocation" => ""); //array to response ajax conection
+
+    protected function insert($table, $dades) {//insert only one row
+        $fields = array_keys($dades);
+        $prepare_values = array_values($dades);
+
+        if ($keys && $values) {
+            $query = "INSERT INTO " . $table . " (";
+            foreach ($fields as $key => $field) {
+                if ($key) {
+                    $query .= ", ";
+                }
+                $query .= $field;
+            }
+            $query .= ") VALUES (";
+            foreach ($fields as $key => $field) {
+                if ($key) {
+                    $query .= ", ";
+                }
+                $query .= "?";
+            }
+            $query .= ")";
+
+            if ($stmt = $this->sql_prepare($query, $prepare_values)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            $this->result["errorCode"] = 1;
+            $this->result["errorMessage"] = "Intento de insert sin valores o nombre de campo";
+            return false;
+        }
+    }
 }
