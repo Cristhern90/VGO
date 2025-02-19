e4a9b4e3f6257312b3b6ee5b97d565148df0a0d2
Cristian Hernan de la Rosa <cristhern20@gmail.com>
2025-02-19 11:29:50 +0100

    style plats list 1

--- Proyect/front/general/platform/list.php
+++ Proyect/front/general/platform/list.php
@@ -1,18 +1,21 @@
 <?php
-$plats = $VGO->select("platform p","p.*, pf.name family","LEFT JOIN platformfamily pf ON pf.IGDB_id = p.PlatformFamily_IGDB_id");
-
-
-print_r($plats);
+$plats = $VGO->select("platform p", "p.*, pf.name family", "LEFT JOIN platformfamily pf ON pf.IGDB_id = p.PlatformFamily_IGDB_id");
 ?>
 <h1>Plataformas</h1>
 <span><button class="btn-secondary" title="Actualizar plataformas" onclick="act_plats()"><i class="fa fa-reload"></i>a</button></span>
+<div class="row">
+    <?php foreach ($plats as $key => $plat) { ?>
+        <div class="col-2">
+            <?= $plat["name"] ?> <?= ($plat["family"] ? "(" . $plat["family"] . ")" : "") ?>
+        </div>
+    <?php } ?>
+</div>
 <script>
     const page = "front/<?= $prepage ?>/<?= $page ?>/ajax/<?= $page ?>.php";
 
     function act_plats() {
         let myArray = new FormData();
-        myArray.append("function", "act_all_plats");
-        console.log(page);
+        myArray.append("function", "download_new_plats");
         conect(page, myArray);
     }
 </script>
